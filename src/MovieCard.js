0b18f8c514f66fb681cb771450d1754b49cb4578
Mohammed Umer <52200381+mohdumer7@users.noreply.github.com>
2023-04-16 12:18:23 +0530

    Added classes

---
+++ src/MovieCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <>
+        <div>Movie Card</div>
+      </>
+    );
+  }
+}
+
+export default MovieCard;
