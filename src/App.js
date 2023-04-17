c3158f0e110bcfeddf0cd9395cc237300f68bf96
Mohammed Umer <52200381+mohdumer7@users.noreply.github.com>
2023-04-17 08:23:17 +0530

    Added Mounts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React from "react";
 import MovieCard from "./MovieCard";
 import "./index.css";
 class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  static getDerivedStateProps() {}
+
+  componentDidMount() {
+    // here is all the fetch calls takes place in the componentdid mount phase.
+    //happens only once
+  }
+
+  shouldComponentUpdate() {
+    //change in state and props trigger rerender?
+    //returns a bool value if render must rerun or not
+  }
+
   render() {
     return (
       <>
@@ -10,7 +26,17 @@ class App extends React.Component {
       </>
     );
   }
+
+  getSnapshotBeforeUpdate() {
+    //sends a snapshot to component did update
+  }
+
+  componentDidUpdate() {
+    // runs after component updates
+    //happens after each render call
+  }
 }
+
 // This is the wau to send default props
 // MovieCard.defaultProps = {
 //   name:"avengers"
